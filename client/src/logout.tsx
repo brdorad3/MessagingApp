d5cdd05ac9cf0bda27120defaf72aa6c085250c1
adis <vunicadis@gmail.com>
2024-06-07 14:38:40 +0200

    f

--- client/src/logout.tsx
+++ client/src/logout.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { UserContext } from './userContext';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
